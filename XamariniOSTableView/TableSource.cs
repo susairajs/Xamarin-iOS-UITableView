b3df1448624906d7acc3dcf378751cff28f30ce5
DelpinSusaiRaj <susairajs@outlook.com>
2017-07-17 18:58:45 +0530

    all files added

---
+++ XamariniOSTableView/TableSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace XamariniOSTableView
+{
+	public class TableSource:UITableViewSource
+	{
+		string[] list;
+
+		public TableSource()
+		{
+		}
+
+		public TableSource(string[] list)
+		{
+			this.list = list;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			var cell = new UITableViewCell(UITableViewCellStyle.Default, "");
+			string item = list[indexPath.Row];
+			cell.TextLabel.Text = item;
+			return cell;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return list.Length;
+		}
+
+		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+		{
+			string item = list[indexPath.Row];
+			UIAlertView alert = new UIAlertView("Selected Item", item,null, "OK");
+
+		}
+
+	}
+}
---
+++ XamariniOSTableView/ViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamariniOSTableView
+{
+	public partial class ViewController : UIViewController
+	{
+		public string[] list = {"Xamarin","Windows","iOS","Android","Visual Studio","Azure","UWP","Xcode",
+			"Cognitive Service" };
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			MyTable.Source = new TableSource(list);
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
